2147abcb9419e8b038c18f2589e1124d5ea54c24
sherifashraf3151 <sherifashraf00000@gmail.com>
2026-01-30 17:01:02 +0200

    Fix Route in Vercel issue

--- src/Components/Post/PostHeader.jsx
+++ src/Components/Post/PostHeader.jsx
@@ -31,7 +31,7 @@ export default function PostHeader({ photo , name , date , userId , postId , cal
               </div> 
             </div>
 
-            {userData._id === userId && 
+            {userData?._id === userId && 
             <Dropdown>
               <DropdownTrigger>
                 <svg className="w-16 cursor-pointer outline-0" xmlns="http://www.w3.org/2000/svg" width={27} height={27} viewBox="0 0 24 24" fill="none" stroke="#b0b0b0" strokeWidth={2} strokeLinecap="square" strokeLinejoin="round"><circle cx={12} cy={12} r={1} /><circle cx={19} cy={12} r={1} /><circle cx={5} cy={12} r={1} /></svg>
